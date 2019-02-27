e309f1b0542e6026f443dc14fbd9bee36ef3b9c7
Dain Miller <miller.dain@gmail.com>
2019-02-27 14:23:57 -0800

    Add commands to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  root to: "feeds#index", defaults: { format: 'xml' }
+  # Homepage has commands you can use in app
+  root to: "landing#home", defaults: { format: 'html' }
+
+  # Core route used to enter commands
   resources :feeds, defaults: { format: 'xml' }
+
+  # Quick access route for easier URL entry by customers
+  get '/f', controller: 'feeds', action: 'index', defaults: { format: 'xml' }
 end
