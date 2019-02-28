31709fd6310ca0ae46f524d505491127369a016c
Dain Miller <miller.dain@gmail.com>
2019-02-27 23:29:09 -0800

    Convert `eps` in Domain Object layer to Episode
    - https://podfeed.it/f?eps_count=2 => eps_count.map { Episode.new }

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,6 +1,8 @@
 class Feed
   attr_accessor :existing_feed, :episodes, :duplicate_guid, :empty_guid, :malformed_audio
 
+  def initialize ; end
+
   def build_state_from_url params
 
     if params[:existing_feed]
@@ -8,7 +10,7 @@ class Feed
     end
 
     if params[:eps]
-      self.episodes = params[:eps]
+      self.episodes = episodes_for(count: params[:eps])
     end
 
     if params[:duplicate_guid]
@@ -24,4 +26,8 @@ class Feed
     end
   end
 
+  private
+  def episodes_for(count:)
+    (1..count).map { Episode.new }
+  end
 end
