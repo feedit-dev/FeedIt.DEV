0b2e37b92457c8d741aec953f3228233bff59df9
Dain Miller <miller.dain@gmail.com>
2019-03-03 21:04:10 -0800

    Update for landing page and refine test feed UX

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,7 +5,7 @@ class FeedsController < ApplicationController
     eps = params[:eps] || NullEps.new.eps
 
     @feed = Feed.new({
-      existing_feed:  params[:existing_feed],
+      existing_feed:  params[:existing_feed] || params[:existing_feeds],
       episodes:       eps.chomp.to_i,
       empty_guid:     params[:empty_guid] || params[:empty_guids],
       duplicate_guid: params[:duplicate_guid] || params[:duplicate_guids]
