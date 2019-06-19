f64d4580b25ab87268454183721e3b197fc360b6
Dain Miller <miller.dain@gmail.com>
2019-06-19 08:05:29 -0700

    WIP JSON

--- app/controllers/json_controller.rb
+++ app/controllers/json_controller.rb
@@ -12,6 +12,16 @@ class JsonController < GeneratorController
         id: 1,
         name: 'Joe Rogan',
         imageUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4b/The_Joe_Rogan_Experience_logo.jpg/220px-The_Joe_Rogan_Experience_logo.jpg'
+      },
+      {
+        id: 2,
+        name: 'Start Here: Web Dev',
+        imageUrl: 'http://i1.sndcdn.com/avatars-000227802710-27eerh-original.jpg'
+      },
+      {
+        id: 3,
+        name: "Start Here: Ruby on Rails",
+        imageUrl: 'http://i1.sndcdn.com/avatars-000227601719-zmd5i2-original.jpg'
       }
     ]
     render json: @shows
